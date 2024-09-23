68e9f0a85106d5d9de376ea5b7e103df00b3db0f
Ahmed Sabry <ahmedsabrihindawi@gmail.com>
2024-09-23 13:52:30 +0300

    change main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-Object.defineProperty(exports, '__esModule', { value: true });
-const core_1 = require('@nestjs/core');
-const app_module_1 = require('./app.module');
-const common_1 = require('@nestjs/common');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
-    new common_1.ValidationPipe({
+    new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,

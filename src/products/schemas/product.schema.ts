8c1e2957db64dd1c4f6dbac77832e2b5ce459704
Ahmed Sabry <ahmedsabrihindawi@gmail.com>
2024-10-07 21:14:55 +0300

    feat(products search):in the schema Adding the text index for multilingual support

--- src/products/schemas/product.schema.ts
+++ src/products/schemas/product.schema.ts
@@ -1,18 +1,14 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Types } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 @Schema({ timestamps: true })
-export class Product {
+export class Product extends Document {
   @Prop({ type: Types.ObjectId, ref: 'User', required: true })
   sellerId: Types.ObjectId;
 
   @Prop({ type: [Types.ObjectId], ref: 'Review' })
   reviews: Types.ObjectId[];
 
-  // @Prop({ type: Types.ObjectId, ref: 'Category' })
-  // categoryId: Types.ObjectId;
-
-  // Reference to Subcategory instead of Category
   @Prop({ type: Types.ObjectId, ref: 'Subcategory', required: true })
   subcategoryId: Types.ObjectId;
 
@@ -55,4 +51,13 @@ export class Product {
   stock: number;
 }
 
+// Create a text index on the `name.en`, `name.ar`, and `description` fields
 export const ProductSchema = SchemaFactory.createForClass(Product);
+
+// Adding the text index for multilingual support
+ProductSchema.index({
+  'name.en': 'text',
+  'name.ar': 'text',
+  'description.en': 'text',
+  'description.ar': 'text',
+});
